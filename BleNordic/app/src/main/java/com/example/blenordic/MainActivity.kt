5084839be35b45e37ed573d12e3887bbc86b5c0d
Joey yang <yty0606@gmail.com>
2022-04-18 15:57:59 +0800

    Feat: Join navigation
    Solution:
    1. As title.
    
    Author: Joey

--- BleNordic/app/src/main/java/com/example/blenordic/MainActivity.kt
+++ BleNordic/app/src/main/java/com/example/blenordic/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.blenordic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.blenordic.ui.theme.BleNordicTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +30,89 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BleNordicTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                ComposeNavigation()
             }
+        }
+    }
+
+}
+
+@Composable
+fun ComposeNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController,
+        startDestination = "first_screen") {
+        composable("first_screen") {
+            FirstScreen(navController = navController)
+        }
+        composable("second_screen") {
+            SecondScreen(navController = navController)
+        }
+        composable("third_screen") {
+            ThirdScreen(navController = navController)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FirstScreen(navController: NavController) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text(
+          text = "First Screen\nTo Second screen",
+          color = Color.Blue,
+          style = TextStyle(textAlign = TextAlign.Center),
+          modifier = Modifier.padding(24.dp).clickable(onClick = {
+              navController.navigate("second_screen")
+          })
+      )
+    }
+}
+
+@Composable
+fun SecondScreen(navController: NavController) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Second Screen\nTo Third screen",
+            color = Color.Blue,
+            style = TextStyle(textAlign = TextAlign.Center),
+            modifier = Modifier.padding(24.dp).clickable(onClick = {
+                navController.navigate("third_screen")
+            })
+        )
+    }
+}
+
+
+@Composable
+fun ThirdScreen(navController: NavController) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Third Screen\nTo First screen",
+            color = Color.Blue,
+            style = TextStyle(textAlign = TextAlign.Center),
+            modifier = Modifier.padding(24.dp).clickable(onClick = {
+                navController.navigate("first_screen")
+            })
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BleNordicTheme {
-        Greeting("Android")
+        ComposeNavigation()
     }
 }
